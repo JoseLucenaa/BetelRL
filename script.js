3499ee5b38ab0f2109dbd75df291c21819cefc32
José Lucena <kleitonjose256@gmail.com>
2024-04-04 14:14:34 -0300

    commit js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const open = document.querySelector("menu-icon")
-const close = document.querySelector("btn-fechar")
+const open = document.querySelector(".menu-icon");
+const menu = document.querySelector(".menu-mobile");
+const close = document.querySelector(".btn-fechar")
 
-open.addEventListener('click', ()=>{
-    close.style.display('block');
-})
+open.addEventListener('click', () => {
+    menu.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+    menu.style.display = 'none';
+});
