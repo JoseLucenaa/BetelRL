c27d1ee1738627447faa5e5f530281367a4a749b
José Lucena <kleitonjose256@gmail.com>
2024-04-05 13:56:30 -0300

    commit

--- connects/connects.js
+++ connects/connects.js
@@ -2,17 +2,38 @@ const open = document.querySelector(".menu-icon");
 const close = document.querySelector(".btn-fechar");
 const menu = document.querySelector(".menu-mobile");
 const back = document.querySelector(".overlay-menu");
+const enc = document.getElementById("encontro")
+const btn = document.querySelector(".btn-s1")
+const txt = document.querySelector(".txt-an")
+const cont = document.querySelector(".img-cont")
 
+let isMenuOpen = false;
 
 open.addEventListener('click', () => { 
-        menu.classList.add('abrir-menu')
+    menu.classList.add('abrir-menu');
+    isMenuOpen = true;
 });
 
-
 menu.addEventListener('click', () => { 
-    menu.classList.remove('abrir-menu')
+    menu.classList.remove('abrir-menu');
+    isMenuOpen = false;
 });
 
 back.addEventListener('click', () => { 
-    menu.classList.remove('abrir-menu')
+    menu.classList.remove('abrir-menu');
+    isMenuOpen = false;
+});
+
+enc.addEventListener('click', () => { 
+    if (isMenuOpen) {
+        cont.style.display = 'block';
+        btn.style.display = 'block';
+        txt.style.display = 'none';
+        isMenuOpen = false;
+    } else {
+        cont.style.display = 'none';
+        btn.style.display = 'none';
+        txt.style.display = 'block';
+        isMenuOpen = true;
+    }
 });
